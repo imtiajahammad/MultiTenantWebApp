2ac4bc8f05c54b22b239385076485cfb845eedca
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-04-30 00:24:16 +0600

    initial application

---
+++ MultiTenantWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiTenantWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
