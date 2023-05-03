6f89383206e36cd7f3e3da2e01d860ef4ff1dc5f
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-05-03 15:52:12 +0600

    signIn and logout action in HomeController

--- MultiTenantWebApp/Controllers/HomeController.cs
+++ MultiTenantWebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MultiTenantWebApp.Interfaces;
 using MultiTenantWebApp.Models;
 
 namespace MultiTenantWebApp.Controllers;
@@ -7,12 +8,60 @@ namespace MultiTenantWebApp.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IAppUserService appUserService;
+    private readonly ITenantService tenantService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IAppUserService appUserService,
+        ITenantService tenantService
+        ,ILogger<HomeController> logger)
     {
+        this.appUserService = appUserService;
+        this.tenantService = tenantService;
         _logger = logger;
     }
 
+    public IActionResult Signin(string emailid = "")
+    {
+        ViewBag.Email = emailid;
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Signin(Signin model)
+    {
+    // checking model state
+    if (ModelState.IsValid)
+    {
+        // checking email at first time
+        if (model.Password is null)
+        {
+            // retrieve tenant information by user email
+            var result = this.appUserService.GetTenantByEmail(model.Email);
+            // if valid email then redirect for password
+            if (result is not null) return Redirect(result + "?emailid=" + model.Email);
+            else // if email is invalid then clear Email-ViewBag to stay same page and get again email
+            {
+                ViewBag.Email = string.Empty;
+                ViewBag.Error = "Provide valid email";
+            }
+        }
+        else // this block for password verification, when user provide password to signin
+        {
+            var result = this.appUserService.Signin(model);
+            if (result is null) // if password is wrong then again provide valid password
+            {
+                ViewBag.Email = model.Email;
+                ViewBag.Error = "Provide valid password";
+            }
+            else return Redirect(result); // if password is valid then portal will open for user access
+        }
+    }
+    else ViewBag.Email = ""; // if email is invalid then clear Email-ViewBag to stay same page and get again email
+    return View();
+    }
+    public IActionResult Logout()
+    {
+        return Redirect("http://localhost:5057");
+    }
     public IActionResult Index()
     {
         return View();
